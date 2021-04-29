ceb91aa825ffd2099fd1d32f5b6e01d255c8307e
Andrzej Dziwiński <adziwinski@hem-e.com>
2021-04-29 16:23:59 +0200

    headersWithSources and headersOnly templates added

---
+++ templates/headersOnly/test_package/test.cpp
@@ -0,0 +1,8 @@
+// include all hpp files with full path eg. <HEM/Observer/Notifier.hpp>
+
+#include <iostream>
+
+int main() {
+	std::cout<<"Hello from {{ name }} package!" << std::endl;
+	return 0;
+}
---
+++ templates/headersWithSources/test_package/test.cpp
@@ -0,0 +1,15 @@
+// include all hpp iles with full path eg. <HEM/Observer/Notifier.hpp>
+#include <HEM/{{ name }}/DumpFile_please_replace_it_with_proper_source_files.hpp>
+
+#include <iostream>
+#include <functional>
+
+std::function<void(void)> action = [](){
+    std::cout << "Hello again from {{name}} package" << std::endl;
+}
+
+int main() {
+	std::cout<<"Hello from {{ name }} package!" << std::endl;
+	sayHello(action);
+	return 0;
+}
