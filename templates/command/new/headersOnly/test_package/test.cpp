53eb0569c4ea98499ddf280fbbb468210aa89578
Andrzej Dziwiński <adziwinski@hem-e.com>
2021-04-30 14:39:16 +0200

    HEM/{{name}} added to inc path

--- templates/command/new/headersOnly/test_package/test.cpp
+++ templates/command/new/headersOnly/test_package/test.cpp
@@ -1,5 +1,7 @@
 // include all hpp files with full path eg. <HEM/Observer/Notifier.hpp>
 
+#include <HEM/{{ name }}/DumpFile_please_replace_it_with_proper_source_files.hpp>
+
 #include <iostream>
 
 int main() {
